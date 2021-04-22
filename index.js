316ee77f4fdd60b9690feca53d0a38d0827eb19d
Rohan Ramakrishnan <rohanrk2009@gmail.com>
2021-04-22 16:07:03 -0700

    gitignore update

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
 const http = require('http');
 
+function getNprimes(n) {
+    const arr = [];
+    let i = 2
+  
+    while (arr.length < n) {
+      if (isPrime(i)) {
+        arr.push(i)
+      }
+      i++
+    } 
+    return arr;
+  
+    function isPrime(n) {
+  
+      if ( n < 2 ) {
+        return false
+      }
+  
+      for ( let i = 2; i <= Math.sqrt(n); i++ ) {
+        if ( n % i === 0 ) {
+            return false;
+        } 
+      }
+      return true
+    }
+  
+}
+
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+    a = getNprimes(1000)
+    response.end("Hello World! " + a[999]);
+
 });
 
 const port = process.env.PORT || 1337;
